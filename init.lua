62a6ef518d1c356940670d6e520569ee932b9e42
Luis F Rocha <luisfrocha@icloud.com>
2023-06-22 00:33:45 -0500

    Removed duplicated setup

--- init.lua
+++ init.lua
@@ -911,29 +911,6 @@ require('barbar').setup({
   -- auto_hide = true
 })
 
-require('telescope').setup {
-  defaults = {
-    file_ignore_patterns = {
-      "node%_modules/.*",
-    },
-  }
-}
-
-vim.api.nvim_create_autocmd("CursorHold", {
-  buffer = bufnr,
-  callback = function()
-    local opts = {
-      focusable = false,
-      close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
-      border = 'rounded',
-      source = 'always',
-      prefix = ' ',
-      scope = 'cursor',
-    }
-    vim.diagnostic.open_float(nil, opts)
-  end
-})
-
 require('nvim-treesitter').setup()
 
 require('nvim-ts-autotag').setup()
