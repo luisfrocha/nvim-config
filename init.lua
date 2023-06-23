bc029fd3e006c1db8544dcf1e63e9525b7794d21
Luis F Rocha <lf_rocha@apple.com>
2023-06-23 17:46:19 -0500

    formatting handled by eslint instead of prettier

--- init.lua
+++ init.lua
@@ -243,7 +243,7 @@ null_ls.setup({
   sources = {
     null_ls.builtins.diagnostics.eslint,  -- eslint or eslint_d
     null_ls.builtins.code_actions.eslint, -- eslint or eslint_d
-    null_ls.builtins.formatting.prettier  -- prettier, eslint, eslint_d, or prettierd
+    null_ls.builtins.formatting.eslint    -- prettier, eslint, eslint_d, or prettierd
   },
 })
 -- disable netrw at the very start of your init.lua
