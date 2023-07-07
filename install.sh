31b38495e517f7da9e71fffc26b54abed3544d19
Luis F Rocha <lf_rocha@apple.com>
2023-07-06 22:08:49 -0600

    Update; added react-extract

--- install.sh
+++ install.sh
@@ -4,33 +4,34 @@ PROGNAME=$0
 option=$1
 
 usage() {
-  cat << EOF >&2
+	cat <<EOF >&2
 Usage $PROGNAME [-i] [-c]
   -i: Install config files
   -c: Clear/uninstall configs files
 EOF
-exit 1
+	exit 1
 }
 
-CURR_DIR=`pwd`
+CURR_DIR=$(pwd)
 case $option in
-  (-c)
-    echo "Uninstalling configs..."
-    rm -rf $HOME/.config/nvim
-    ;;
-  (-i)
-    echo "Installing configs..."
-    pnpm add -g neovim
-    brew update && \
-    brew upgrade && \
-    brew tap homebrew/cask-fonts
-    brew install \
-      neovim \
-      font-hack-nerd-font
-    mkdir -p $HOME/.config
-    ln -s $CURR_DIR $HOME/.config/nvim
-    echo "Installation complete."
-    ;;
-  (*) usage
+-c)
+	echo "Uninstalling configs..."
+	rm -rf $HOME/.config/nvim
+	;;
+-i)
+	echo "Installing configs..."
+	pnpm add -g neovim
+	brew update &&
+		brew upgrade &&
+		brew tap homebrew/cask-fonts
+	brew install \
+		neovim \
+		font-hack-nerd-font \
+		ctags
+	mkdir -p $HOME/.config
+	ln -s $CURR_DIR $HOME/.config/nvim
+	echo "Installation complete."
+	;;
+*) usage ;;
 esac
 shift "$((OPTIND - 1))"
