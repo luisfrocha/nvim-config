9b91f5c7fdcc7a2a323fdd37b35697a5422295a4
Luis F Rocha <lf_rocha@apple.com>
2023-07-13 18:08:08 -0600

    Formatting

--- init.lua
+++ init.lua
@@ -7,8 +7,9 @@ vim.node_host_prog = "/Users/luisrocha/.nvm/versions/node/v18.16.0/bin:node"
 
 -- Enable mouse movements
 vim.opt.mousemoveevent = true
+
+-- Wrap lines and indent
 vim.opt.linebreak = true
 vim.opt.wrap = true
 vim.opt.showbreak = "↳ "
-vim.opt.cpo = "n"
 vim.opt.breakindent = true
