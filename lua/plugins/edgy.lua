21e93df91ab0c64dac8534b944de5d1460596ab0
Luis F Rocha <lf_rocha@apple.com>
2023-07-25 13:51:39 -0600

    Added Edgy, hook, and windows plugins

---
+++ lua/plugins/edgy.lua
@@ -0,0 +1,34 @@
+return {
+  {
+    "folke/edgy.nvim",
+    event = "VeryLazy",
+    init = function()
+      vim.opt.laststatus = 3
+      vim.opt.splitkeep = "screen"
+    end,
+    opts = {
+      animate = { enable = false },
+      bottom = {
+        {
+          ft = "lazyterm",
+          title = "LazyTerm",
+          size = { height = 0.1 },
+          filter = function(buf)
+            return not vim.b[buf].lazyterm_cmd
+          end,
+        },
+        "Trouble",
+        { ft = "qf", title = "QuickFix" },
+        {
+          ft = "help",
+          size = { height = 20 },
+          -- only show help buffers
+          filter = function(buf)
+            return vim.bo[buf].buftype == "help"
+          end,
+        },
+        { ft = "spectre_panel", size = { height = 0.4 } },
+      },
+    },
+  },
+}
