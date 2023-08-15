7443d8bdb85e9345eeb7d4f9000e2f936328f670
Luis F Rocha <lf_rocha@apple.com>
2023-08-15 08:53:12 -0500

    Spectre start setting; moved out of edgy

---
+++ lua/plugins/nvim-spectre.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "nvim-pack/nvim-spectre",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    opts = {
+      is_insert_mode = true,
+      live_update = true,
+    },
+  },
+}
