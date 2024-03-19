3195dab42e8bbe8abdc4b9e14d71d382bce9ce08
Luis F Rocha <luisfrocha@icloud.com>
2024-03-19 07:19:51 -0500

    Spectre adjustments

--- lua/plugins/nvim-spectre.lua
+++ lua/plugins/nvim-spectre.lua
@@ -8,13 +8,16 @@ return {
       {
         "<C-S-F>",
         '<cmd>lua require("spectre").open_visual({select_word=true,is_insert_mode=true,live_update=true})<CR>',
-        desc = "Symbols Outline",
+        desc = "Global Search",
       },
     },
     opts = {
       is_insert_mode = true,
       select_word = true,
       live_update = true,
+      lnum_for_results = true,
+      color_devicons = true,
+      default = { find = { options = {} } },
     },
   },
 }
