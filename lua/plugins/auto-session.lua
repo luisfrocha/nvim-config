e598e2816e619ea31f30473459c04b230e3e13c6
Luis Rocha <lf_rocha@apple.com>
2024-11-19 08:48:23 -0600

    Language tools

---
+++ lua/plugins/auto-session.lua
@@ -0,0 +1,24 @@
+return {
+  "rmagatti/auto-session",
+  config = function()
+    local auto_session = require("auto-session")
+
+    auto_session.setup({
+      auto_session_enabled = false,
+      auto_session_enable_last_session = false,
+      auto_session_suppress_dirs = { "~/", "~/Dev", "~/Downloads", "~/Documents", "~/Desktop/" },
+      post_restore_cmds = function()
+        local buffer_list = vim.fn.execute("ls t")
+        local buf_names = vim.split(buffer_list, "\n")
+        if #buf_names == 0 then
+          vim.cmd([[Alpha]])
+        end
+      end,
+    })
+
+    local keymap = vim.keymap
+    vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
+    keymap.set("n", "<leader>wr", "<cmd>SessionRestore<CR>", { desc = "Restore session for cwd" })
+    keymap.set("n", "<leader>ws", "<cmd>SessionSave<CR>", { desc = "Save session for auto session root dir" })
+  end,
+}
---
+++ lua/plugins/mason.lua
@@ -0,0 +1,35 @@
+return {
+  "williamboman/mason.nvim",
+dependencies = {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    "williamboman/mason-lspconfig.nvim",
+    "neovim/nvim-lspconfig",
+  },
+  config = function()
+    -- import mason
+    local mason = require("mason")
+
+    -- enable mason and configure icons
+    mason.setup({
+      ui = {
+        icons = {
+          package_installed = "✓",
+          package_pending = "➜",
+          package_uninstalled = "✗",
+        },
+      },
+    })
+    local mason_tool_installer = require("mason-tool-installer")
+    mason_tool_installer.setup({
+      ensure_installed = {
+        "prettierd", -- prettier formatter
+        "stylua", -- lua formatter
+        "eslint-lsp", -- js linter
+        "yaml-language-server", -- handle yaml files
+        "stylelint",
+        "volar",
+        "intelephense",
+      },
+    })
+  end,
+}
