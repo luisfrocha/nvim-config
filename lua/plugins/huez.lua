dbf324cc3b1b3dfdb9a963fb797d98c22281fcd2
Luis Rocha <lf_rocha@apple.com>
2024-11-20 21:49:50 -0600

    Set up colorscheme right

--- lua/plugins/huez.lua
+++ lua/plugins/huez.lua
@@ -3,6 +3,7 @@ return {
   -- if you want registry related features, uncomment this
   -- import = "huez-manager.import"
   branch = "stable",
+  enabled = false,
   event = "UIEnter",
   config = function()
     require("huez").setup({})
