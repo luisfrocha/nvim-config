3b3e66f1d302631244e8c2dd0962e14f39f9ebed
Luis Rocha <lf_rocha@apple.com>
2025-04-17 20:06:46 -0500

    Adjustments

--- init.lua
+++ init.lua
@@ -10,3 +10,17 @@ vim.cmd([[
 ]])
 vim.o.guifont = "Victor Mono,Hack Nerd Font:h16:i"
 vim.g.transparent_enabled = true
+
+if vim.g.neovide then
+  vim.g.neovide_opacity = 0.9
+  vim.opt.linespace = -4
+  vim.g.neovide_hide_mouse_when_typing = true
+  vim.g.neovide_underline_automatic_scaling = true
+  vim.g.neovide_remember_window_size = true
+  vim.g.neovide_input_macos_option_is_meta = true
+  vim.g.neovide_cursor_trail_size = 1
+  vim.g.neovide_cursor_smooth_blink = true
+  vim.g.neovide_cursor_vfx_mode = "pixiedust"
+  vim.g.neovide_cursor_vfx_particle_lifetime = 2
+  vim.g.neovide_cursor_vfx_particle_density = 19.0
+end
