b0b75e056a4f3c0dc98e6a2dc65cacfd1b2622a6
Luis Rocha <lf_rocha@apple.com>
2025-11-21 12:57:08 -0600

    Updates

--- lua/plugins/productivity.lua
+++ lua/plugins/productivity.lua
@@ -46,9 +46,7 @@ return {
     event = { "InsertLeave", "TextChanged" },
     opts = {
       enabled = true,
-      execution_message = {
-        enabled = false,
-      },
+      -- execution_message removed - this option has been deprecated
       events = { "InsertLeave", "TextChanged" },
       conditions = {
         exists = true,
@@ -70,13 +68,13 @@ return {
     },
     keys = {
       { "<leader>p", function() require("yanky.telescope").yank_history() end, desc = "Open Yank History" },
-      { "y", "<Plug>(YankyYank)", mode = { "n", "x" }, desc = "Yank text" },
-      { "p", "<Plug>(YankyPutAfter)", mode = { "n", "x" }, desc = "Put yanked text after cursor" },
-      { "P", "<Plug>(YankyPutBefore)", mode = { "n", "x" }, desc = "Put yanked text before cursor" },
-      { "gp", "<Plug>(YankyGPutAfter)", mode = { "n", "x" }, desc = "Put yanked text after selection" },
-      { "gP", "<Plug>(YankyGPutBefore)", mode = { "n", "x" }, desc = "Put yanked text before selection" },
-      { "[y", "<Plug>(YankyCycleForward)", desc = "Cycle forward through yank history" },
-      { "]y", "<Plug>(YankyCycleBackward)", desc = "Cycle backward through yank history" },
+      { "y",         "<Plug>(YankyYank)",                                      mode = { "n", "x" },                         desc = "Yank text" },
+      { "p",         "<Plug>(YankyPutAfter)",                                  mode = { "n", "x" },                         desc = "Put yanked text after cursor" },
+      { "P",         "<Plug>(YankyPutBefore)",                                 mode = { "n", "x" },                         desc = "Put yanked text before cursor" },
+      { "gp",        "<Plug>(YankyGPutAfter)",                                 mode = { "n", "x" },                         desc = "Put yanked text after selection" },
+      { "gP",        "<Plug>(YankyGPutBefore)",                                mode = { "n", "x" },                         desc = "Put yanked text before selection" },
+      { "[y",        "<Plug>(YankyCycleForward)",                              desc = "Cycle forward through yank history" },
+      { "]y",        "<Plug>(YankyCycleBackward)",                             desc = "Cycle backward through yank history" },
     },
   },
 
@@ -85,14 +83,14 @@ return {
     "mrjones2014/smart-splits.nvim",
     lazy = false,
     keys = {
-      { "<C-h>", function() require("smart-splits").move_cursor_left() end, desc = "Move to left split" },
-      { "<C-j>", function() require("smart-splits").move_cursor_down() end, desc = "Move to below split" },
-      { "<C-k>", function() require("smart-splits").move_cursor_up() end, desc = "Move to above split" },
-      { "<C-l>", function() require("smart-splits").move_cursor_right() end, desc = "Move to right split" },
-      { "<C-Up>", function() require("smart-splits").resize_up() end, desc = "Resize split up" },
-      { "<C-Down>", function() require("smart-splits").resize_down() end, desc = "Resize split down" },
-      { "<C-Left>", function() require("smart-splits").resize_left() end, desc = "Resize split left" },
-      { "<C-Right>", function() require("smart-splits").resize_right() end, desc = "Resize split right" },
+      { "<C-h>",     function() require("smart-splits").move_cursor_left() end,  desc = "Move to left split" },
+      { "<C-j>",     function() require("smart-splits").move_cursor_down() end,  desc = "Move to below split" },
+      { "<C-k>",     function() require("smart-splits").move_cursor_up() end,    desc = "Move to above split" },
+      { "<C-l>",     function() require("smart-splits").move_cursor_right() end, desc = "Move to right split" },
+      { "<C-Up>",    function() require("smart-splits").resize_up() end,         desc = "Resize split up" },
+      { "<C-Down>",  function() require("smart-splits").resize_down() end,       desc = "Resize split down" },
+      { "<C-Left>",  function() require("smart-splits").resize_left() end,       desc = "Resize split left" },
+      { "<C-Right>", function() require("smart-splits").resize_right() end,      desc = "Resize split right" },
     },
   },
 
@@ -101,9 +99,9 @@ return {
     "mg979/vim-visual-multi",
     event = "VeryLazy",
     keys = {
-      { "<C-n>", desc = "Select word under cursor" },
+      { "<C-n>",    desc = "Select word under cursor" },
       { "<C-Down>", desc = "Create cursor below" },
-      { "<C-Up>", desc = "Create cursor above" },
+      { "<C-Up>",   desc = "Create cursor above" },
     },
   },
 }
