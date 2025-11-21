e212006854c89477e918c1aa0ab991df2406ad45
Luis F Rocha <luisfrocha@icloud.com>
2025-11-20 21:16:29 -0600

    Remove Code Companion

--- lua/plugins/code-companion.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "https://github.pie.apple.com/AI-for-Devs-Community/code-companion.nvim",
-  opts = {},
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-treesitter/nvim-treesitter",
-  },
-}
